257c84fb65a12759f16c44069c37c196fc333295
Rustem Akhmetov <Rustem_Akhmetov@epam.com>
2020-09-23 18:31:52 +0200

    removed startup package content; switched to TS

---
+++ gatsby-playground/src/pages/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Index = () => {
+  return (
+    <div>
+      <p>Home / Index page</p>
+    </div>
+  );
+};
+
+export default Index;
---
+++ gatsby-playground/src/pages/page-2.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { Link } from "gatsby";
+
+const SecondPage = () => <h2>Page 2</h2>;
+
+export default SecondPage;
