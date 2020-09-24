03e89ec70352ecb541b5df816a5360e57dd54494
Rustem Akhmetov <Rustem_Akhmetov@epam.com>
2020-09-24 11:26:44 +0200

    few more prettier rules and links between pages

--- gatsby-playground/src/pages/index.tsx
+++ gatsby-playground/src/pages/index.tsx
@@ -1,9 +1,11 @@
-import React from "react";
+import { Link } from 'gatsby';
+import React from 'react';
 
 const Index = () => {
   return (
     <div>
       <p>Home / Index page</p>
+      <Link to="page-2">Page 2</Link>
     </div>
   );
 };
--- gatsby-playground/src/pages/page-2.tsx
+++ gatsby-playground/src/pages/page-2.tsx
@@ -1,6 +1,11 @@
-import React from "react";
-import { Link } from "gatsby";
+import React from 'react';
+import { Link } from 'gatsby';
 
-const SecondPage = () => <h2>Page 2</h2>;
+const SecondPage = () => (
+  <div>
+    <h2>Page 2</h2>
+    <Link to="/">Home page</Link>
+  </div>
+);
 
 export default SecondPage;
