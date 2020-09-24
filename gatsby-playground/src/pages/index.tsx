73ee4522142f2a27e19a53a68ed23dc0ad809c78
Rustem Akhmetov <Rustem_Akhmetov@epam.com>
2020-09-24 14:11:30 +0200

    rendering blog posts as json

--- gatsby-playground/src/pages/index.tsx
+++ gatsby-playground/src/pages/index.tsx
@@ -1,12 +1,19 @@
 import { Link } from 'gatsby';
 import React from 'react';
 import Layout from '../components/layout';
+import usePosts from '../hooks/usePosts';
 
 const Index = () => {
+  const posts = usePosts();
+
   return (
     <Layout>
-      <h1>Home / Index page</h1>
-      <Link to="/about">To About page</Link>
+      <h2>Hi all, fancy title here</h2>
+      <hr></hr>
+      <h3>Blog posts:</h3>
+      {posts.map(post => (
+        <pre>{JSON.stringify(post, null, 2)}</pre>
+      ))}
     </Layout>
   );
 };
